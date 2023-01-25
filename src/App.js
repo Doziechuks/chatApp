9614359f91b6dd022bea01418131f921ea37f22b
doziechuks <doziechuks1010@gmail.com>
2023-01-25 08:45:13 +0100

    setup my appjs page and styled it

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import classes from './App.module.css';
 
 function App() {
   return (
-    <div className={classes.App}>
-     hello
+    <div className={classes.app}>
+      <div className={classes.background} />
+     <div className={classes.components}>
+      hello
+     </div>
     </div>
   );
 }
