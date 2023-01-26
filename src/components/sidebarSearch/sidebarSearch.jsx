b4690fcaa4134f79b4adfa197177821e578c655d
doziechuks <doziechuks1010@gmail.com>
2023-01-26 22:47:44 +0100

    created a search function for friends

--- src/components/sidebarSearch/sidebarSearch.jsx
+++ src/components/sidebarSearch/sidebarSearch.jsx
@@ -1,20 +1,109 @@
-import classes from './sidebarSearch.module.css';
+import classes from "./sidebarSearch.module.css";
 import { AiOutlineSearch } from "react-icons/ai";
-import FormInput from '../formInput/formInput.component';
+import { useState } from "react";
 
-const SidebarSearch = () => {
-  return ( 
+import { collection, query, where, getDocs, getDoc, doc, setDoc, updateDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
+
+import { connect } from "react-redux";
+import { selectCurrentUser } from "../../redux/user/userSelector";
+import { createStructuredSelector } from "reselect";
+
+const SidebarSearch = ({ currentUser }) => {
+  const [username, setUsername] = useState("");
+  const [foundFriend, setFoundFriend] = useState(null);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async () => {
+    const queryRef = query(
+      collection(db, "myChatAppUsers"),
+      where("username", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(queryRef);
+      querySnapshot.forEach((doc) => {
+        setFoundFriend(doc.data());
+      });
+    } catch (error) {
+      console.log(error.message);
+      setError(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  const handleSelectFoundFriend = async () => {
+    const combinedId = currentUser.uid + foundFriend.uid;
+    try {
+      const res = await getDoc(doc(db, 'chats', combinedId));
+      if(!res.exists()){
+        await setDoc(doc(db, 'chats', combinedId), {messages: []});
+        
+        //update currentUsers 'myUserChats' doc
+        await updateDoc(doc(db, "myUserChats", currentUser.uid), {
+          uid: foundFriend.uid,
+          username: foundFriend.username,
+          photoURL: foundFriend.photoURL,
+          email: foundFriend.email,
+          createdDate: foundFriend.createdDate,
+          combinedId,
+          date: serverTimestamp()
+        });
+
+        //update foundFriends 'myUserChats' doc
+        await updateDoc(doc(db, "myUserChats", foundFriend.uid), {
+          uid: currentUser.uid,
+          username: currentUser.username,
+          photoURL: currentUser.photoURL,
+          email: currentUser.email,
+          createdDate: currentUser.createdDate,
+          combinedId,
+          date: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+    setFoundFriend(null);
+    setUsername('');
+  };
+
+  console.log(foundFriend);
+  return (
     <div className={classes.container}>
       <div className={classes.searchbox}>
-        <AiOutlineSearch className={classes.searchIcon} />
-        <input type="text" placeholder='Search or start a new chat' className={classes.search} />
+        <AiOutlineSearch
+          className={classes.searchIcon}
+          onClick={handleSearch}
+        />
+        <input
+          type="text"
+          placeholder="Search or start a new chat"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={handleKey}
+          className={classes.search}
+        />
       </div>
-      <div className={classes.founduser}>
-        <img src="" alt="photo" className={classes.usePhoto} />
-        <p>chuka</p>
-      </div>
+      {error && <p>user not founder</p>}
+      {foundFriend && (
+        <div className={classes.founduser} onClick={handleSelectFoundFriend}>
+          <img
+            src={foundFriend.photoURL}
+            alt="photo"
+            className={classes.userPhoto}
+          />
+          <p>{foundFriend.username}</p>
+        </div>
+      )}
     </div>
-   );
-}
- 
-export default SidebarSearch;
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
+export default connect(mapStateToProps)(SidebarSearch);
