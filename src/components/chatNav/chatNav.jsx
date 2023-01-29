5f2750bc09bdf976bf1c976af77242792ea6aac0
doziechuks <doziechuks1010@gmail.com>
2023-01-29 18:31:00 +0100

    removed all styles not needed

--- src/components/chatNav/chatNav.jsx
+++ src/components/chatNav/chatNav.jsx
@@ -5,22 +5,23 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectFriendsChat } from "../../redux/friendsChat/friendsChatSelector";
 
-const ChatNav = ({friend}) => {
-  const {username, photoURL} = friend;
+const ChatNav = ({ friend }) => {
+  const { username, photoURL } = friend;
+  console.log(friend);
   return (
     <div className={classes.container}>
-          <div className={classes.friendInfo}>
-            <img src={photoURL} alt="photo" className={classes.friendPhoto} />
-            <div className={classes.username}>{username}</div>
-          </div>
-          <div className={classes.dotBox}>
-            <BsThreeDotsVertical className={classes.dots} />
-          </div>
+      <div className={classes.friendInfo}>
+        <img src={photoURL} alt="photo" className={classes.friendPhoto} />
+        <div className={classes.username}>{username}</div>
+      </div>
+      <div className={classes.dotBox}>
+        <BsThreeDotsVertical className={classes.dots} />
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
-  friend: selectFriendsChat
+  friend: selectFriendsChat,
 });
 export default connect(mapStateToProps)(ChatNav);
